12480c940aebb4a3379fe0d3dfeef1e84b741ee1
Iman Khoshabi <iman.neofight@gmail.com>
2023-12-24 21:22:11 +0100

    Change the debug panel font to Roboto (for better readability)

--- lib/widgets/debug_panel.dart
+++ lib/widgets/debug_panel.dart
@@ -16,84 +16,90 @@ class DebugPanel extends StatelessWidget {
     }
     return BlocBuilder<GameCubit, GameState>(
       builder: (context, state) {
-        return SafeArea(
-          child: Container(
-            margin: const EdgeInsets.all(8.0),
-            padding: const EdgeInsets.all(8.0),
-            decoration: BoxDecoration(
-              color: Colors.black87,
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Debug Panel:',
-                  style: TextStyle(fontSize: 18),
-                ),
-                const SizedBox(height: 24),
-                Text('timePassed: ${state.timePassed.toStringAsFixed(2)}'),
-                const SizedBox(height: 12),
-                Text(
-                    'difficulty (${GameConfigs.difficultyInitialToPeakDuration}s): %${(state.difficulty * 100).toInt()}'),
-                const SizedBox(height: 4),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text('0.0'),
-                    const SizedBox(width: 4),
-                    SizedBox(
-                      width: 200,
-                      child: LinearProgressIndicator(value: state.difficulty),
-                    ),
-                    const SizedBox(width: 4),
-                    const Text('1.0'),
-                  ],
-                ),
-                const SizedBox(height: 12),
-                Text('spawnEvery: ${state.spawnOrbsEvery.toStringAsFixed(2)}'),
-                const SizedBox(height: 4),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text('${GameConfigs.orbsSpawnEveryInitial}'),
-                    const SizedBox(width: 4),
-                    SizedBox(
-                      width: 200,
-                      child: LinearProgressIndicator(
-                        value: 1 -
-                            ((state.spawnOrbsEvery -
-                                    GameConfigs.orbsSpawnEveryPeak) /
-                                (GameConfigs.orbsSpawnEveryInitial -
-                                    GameConfigs.orbsSpawnEveryPeak)),
+        return DefaultTextStyle(
+          style: const TextStyle(
+            color: Colors.white,
+            fontFamily: 'RobotoMono',
+          ),
+          child: SafeArea(
+            child: Container(
+              margin: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.black87,
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Debug Panel:',
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  const SizedBox(height: 24),
+                  Text('timePassed: ${state.timePassed.toStringAsFixed(2)}'),
+                  const SizedBox(height: 12),
+                  Text(
+                      'difficulty (${GameConfigs.difficultyInitialToPeakDuration}s): %${(state.difficulty * 100).toInt()}'),
+                  const SizedBox(height: 4),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('0.0'),
+                      const SizedBox(width: 4),
+                      SizedBox(
+                        width: 200,
+                        child: LinearProgressIndicator(value: state.difficulty),
                       ),
-                    ),
-                    const SizedBox(width: 4),
-                    const Text('${GameConfigs.orbsSpawnEveryPeak}'),
-                  ],
-                ),
-                const SizedBox(height: 12),
-                Text(
-                    'Orbs Move Speed: ${state.spawnOrbsMoveSpeedRange.simpleFormatInt}'),
-                Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text(GameConfigs.orbsMoveSpeedInitial.simpleFormatInt),
-                    const SizedBox(width: 4),
-                    SizedBox(
-                      width: 200,
-                      child: RangeProgressIndicator(
-                        min: GameConfigs.orbsMoveSpeedInitial.min,
-                        max: GameConfigs.orbsMoveSpeedPeak.max,
-                        range: state.spawnOrbsMoveSpeedRange,
+                      const SizedBox(width: 4),
+                      const Text('1.0'),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  Text('spawnEvery: ${state.spawnOrbsEvery.toStringAsFixed(2)}'),
+                  const SizedBox(height: 4),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text('${GameConfigs.orbsSpawnEveryInitial}'),
+                      const SizedBox(width: 4),
+                      SizedBox(
+                        width: 200,
+                        child: LinearProgressIndicator(
+                          value: 1 -
+                              ((state.spawnOrbsEvery -
+                                      GameConfigs.orbsSpawnEveryPeak) /
+                                  (GameConfigs.orbsSpawnEveryInitial -
+                                      GameConfigs.orbsSpawnEveryPeak)),
+                        ),
                       ),
-                    ),
-                    const SizedBox(width: 4),
-                    Text(GameConfigs.orbsMoveSpeedPeak.simpleFormatInt),
-                  ],
-                ),
-              ],
+                      const SizedBox(width: 4),
+                      const Text('${GameConfigs.orbsSpawnEveryPeak}'),
+                    ],
+                  ),
+                  const SizedBox(height: 12),
+                  Text(
+                      'Orbs Move Speed: ${state.spawnOrbsMoveSpeedRange.simpleFormatInt}'),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(GameConfigs.orbsMoveSpeedInitial.simpleFormatInt),
+                      const SizedBox(width: 4),
+                      SizedBox(
+                        width: 200,
+                        child: RangeProgressIndicator(
+                          min: GameConfigs.orbsMoveSpeedInitial.min,
+                          max: GameConfigs.orbsMoveSpeedPeak.max,
+                          range: state.spawnOrbsMoveSpeedRange,
+                        ),
+                      ),
+                      const SizedBox(width: 4),
+                      Text(GameConfigs.orbsMoveSpeedPeak.simpleFormatInt),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
