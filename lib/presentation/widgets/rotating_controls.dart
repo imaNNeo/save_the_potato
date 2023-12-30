38c90410d6fe5c96a803203f6604d9a8c9ec96e8
Iman Khoshabi <iman.neofight@gmail.com>
2023-12-30 15:28:00 +0100

    Implement guide animation

--- lib/presentation/widgets/rotating_controls.dart
+++ lib/presentation/widgets/rotating_controls.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 
 class RotationControls extends StatelessWidget {
   const RotationControls({
@@ -20,6 +21,10 @@ class RotationControls extends StatelessWidget {
   Widget build(BuildContext context) {
     const bottomPadding = 40.0;
     const edgePadding = 44.0;
+    const iconSize = 88.0;
+    const startOpacity = 0.3;
+    const endOpacity = 1.0;
+    const tapAndHoldText = 'TAPE & HOLD';
     return Row(
       children: [
         Expanded(
@@ -30,22 +35,45 @@ class RotationControls extends StatelessWidget {
             child: Container(
               height: double.infinity,
               color: Colors.transparent,
-              child: showGuide ? const Align(
-                alignment: Alignment.bottomLeft,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    bottom: bottomPadding,
-                    left: edgePadding,
-                  ),
-                  child: SafeArea(
-                    child: Icon(
-                      Icons.undo_rounded,
-                      size: 80,
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ) : const SizedBox(),
+              child: showGuide
+                  ? Align(
+                      alignment: Alignment.bottomLeft,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: bottomPadding,
+                          left: edgePadding,
+                        ),
+                        child: SafeArea(
+                          child: const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.undo_rounded,
+                                size: iconSize,
+                                color: Colors.white70,
+                              ),
+                              Text(tapAndHoldText),
+                            ],
+                          )
+                              .animate(
+                                autoPlay: true,
+                                onPlay: (controller) => controller.repeat(
+                                  reverse: true,
+                                ),
+                              )
+                              .fade(
+                                begin: startOpacity,
+                                end: endOpacity,
+                                delay: const Duration(milliseconds: 1400),
+                                curve: Curves.easeOutExpo,
+                                duration: const Duration(
+                                  milliseconds: 700,
+                                ),
+                              ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
             ),
           ),
         ),
@@ -57,22 +85,46 @@ class RotationControls extends StatelessWidget {
             child: Container(
               height: double.infinity,
               color: Colors.transparent,
-              child: showGuide ? const Align(
-                alignment: Alignment.bottomRight,
-                child: Padding(
-                  padding: EdgeInsets.only(
-                    bottom: bottomPadding,
-                    right: edgePadding,
-                  ),
-                  child: SafeArea(
-                    child: Icon(
-                      Icons.redo_rounded,
-                      size: 80,
-                      color: Colors.white70,
-                    ),
-                  ),
-                ),
-              ): const SizedBox(),
+              child: showGuide
+                  ? Align(
+                      alignment: Alignment.bottomRight,
+                      child: Padding(
+                        padding: const EdgeInsets.only(
+                          bottom: bottomPadding,
+                          right: edgePadding,
+                        ),
+                        child: SafeArea(
+                          child: const Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Icon(
+                                Icons.redo_rounded,
+                                size: iconSize,
+                                color: Colors.white70,
+                              ),
+                              Text(tapAndHoldText),
+                            ],
+                          )
+                              .animate(
+                                autoPlay: true,
+                                delay: const Duration(milliseconds: 2100),
+                                onPlay: (controller) => controller.repeat(
+                                  reverse: true,
+                                ),
+                              )
+                              .fade(
+                                begin: startOpacity,
+                                end: endOpacity,
+                                delay: const Duration(milliseconds: 1400),
+                                curve: Curves.easeOutExpo,
+                                duration: const Duration(
+                                  milliseconds: 700,
+                                ),
+                              ),
+                        ),
+                      ),
+                    )
+                  : const SizedBox(),
             ),
           ),
         ),
