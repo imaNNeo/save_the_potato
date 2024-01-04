2f983a482e6d6b568c1e5f48c25e5ac7af14e428
Iman Khoshabi <iman.neofight@gmail.com>
2024-01-04 21:20:00 +0100

    Fix presentation score in leaderboard

--- lib/domain/app_utils.dart
+++ lib/domain/app_utils.dart
@@ -2,6 +2,6 @@ class AppUtils {
   static String getHighScoreRepresentation(int highScoreMilliseconds) {
     final minutes = highScoreMilliseconds ~/ 60000;
     final seconds = (highScoreMilliseconds % 60000) ~/ 1000;
-    return '$minutes:$seconds';
+    return '${minutes.toString().padLeft(2, '0')}:${seconds.toString().padLeft(2, '0')}';
   }
 }
